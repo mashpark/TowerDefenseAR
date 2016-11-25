fecea31fd0a592990c4f407f6eb3e78165346475
Miyoung Park <mashpark123@gmail.com>
2016-11-25 14:51:03 +0900

    Add asset and project setting's folder.

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gun : MonoBehaviour 
+{
+    public Camera camera;
+    public GameObject particlePrefab;
+    ParticleSystem ps;
+	// Use this for initialization
+	void Start () 
+	{
+        ps = particlePrefab.GetComponent<ParticleSystem>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+        if (Input.GetButtonDown("Fire1"))
+        {
+            RaycastHit hit;
+            Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+ //           Ray ray = new Ray(camera.transform.position, camera.transform.forward);
+            if (Physics.Raycast(ray, out hit)) {
+                Transform objectHit = hit.transform;
+                Debug.Log("hit name : " + hit.transform.name);
+                //GameObject effect = Instantiate(particlePrefab);
+                particlePrefab.transform.position = hit.point;
+
+                if (objectHit.transform.tag.Contains("Drone"))
+                {
+                    Destroy(objectHit.gameObject);
+                }
+                else
+                {
+                    ps.Stop();
+                    ps.Play();
+                }
+
+
+
+
+
+            }
+        }
+	}
+
+}
---
+++ Assets/Scripts/MYDrone.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MYDrone : MonoBehaviour 
+{
+    public GameObject target;
+    NavMeshAgent agent;
+	// Use this for initialization
+	void Start () 
+	{
+        agent = GetComponent<NavMeshAgent>();
+        agent.destination = target.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	    
+	}
+}
